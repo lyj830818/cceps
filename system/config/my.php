a1d7a808c31adf81b147a510e2459b049f3dd634
lyj <29422882@qq.com>
2018-03-28 18:56:58 +0800

    test

--- system/config/my.php
+++ system/config/my.php
@@ -1,6 +1,6 @@
 <?php
 $config->installed    = true;	
-$config->debug        = false;	
+$config->debug        = true;	
 $config->db->host     = '127.0.0.1';	
 $config->db->port     = '3306';	
 $config->db->name     = 'chanzhi';	
