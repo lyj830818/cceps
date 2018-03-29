fae7c30f34eb801d8795a9d94e0c6f93bdcd9d54
lyj <29422882@qq.com>
2018-03-29 14:55:26 +0800

    add templates

--- system/tmp/template/default/common/header.lite.html.php
+++ system/tmp/template/default/common/header.lite.html.php
@@ -1,111 +1,36 @@
-{if(!defined("RUN_MODE"))} {!die()} {/if}
-{$extView=$control->getExtViewFile(TPL_ROOT . 'common/header.lite.html.php')}
-{if($extView)} {include $extView} {@return helper::cd()} {/if}
-{$sysURL=rtrim($sysURL, '/')}
-{if(isset($mobileURL))} {$mobileURL=ltrim($mobileURL, '/')} {/if}
 <!DOCTYPE html>
-<html xmlns:wb="http://open.weibo.com/wb" lang='{$app->getClientLang()}' class='m-{$thisModuleName} m-{$thisModuleName}-{$thisMethodName}'>
-<head profile="http://www.w3.org/2005/10/profile">
-  <meta charset="utf-8">
-  <meta name="renderer" content="webkit">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta http-equiv="Cache-Control"  content="no-transform">
-  {if($app->getClientLang() == 'en')}
-  <meta name="Generator" content="Zsite{$config->version} www.zsite.net'">
-  {else}
-  <meta name="Generator" content="chanzhi{$config->version} www.chanzhi.org'">
-  {/if}
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  {if(isset($mobileURL))}
-    <link rel="alternate" media="only screen and (max-width: 640px)" href="{$sysURL}/{$mobileURL}">
-  {/if}
-
-  {if(isset($sourceURL))}
-    <link rel="canonical" href="{$sysURL}/{$sourceURL}" >
-  {else}
-    {if(isset($canonicalURL))} <link rel="canonical" href="{$sysURL}/{$canonicalURL}" > {/if}
-  {/if}
-  {if($thisModuleName == 'user' and $thisMethodName == 'deny')} <meta http-equiv='refresh' content="5;url='{$url= helper::createLink('index')}'"> {/if} 
-  {if(!isset($title))}   {$title    = ''} {/if}
-  {if(!empty($title))}   {$title   .= $lang->minus} {/if}
-  {if(empty($keywords))} {$keywords = $config->site->keywords} {/if}
-  {if(empty($desc))}     {$desc     = $config->site->desc} {/if}
-
-  {!html::title($title . $config->site->name)}
-  {!html::meta('keywords', $keywords)}
-  {!html::meta('description', $desc)}
-  {if(isset($config->site->meta))}{$config->site->meta}{/if}
-
-  {!js::exportConfigVars()}
-  {!js::set('theme', array('template' => CHANZHI_TEMPLATE, 'theme' => CHANZHI_THEME, 'device' => $app->clientDevice))}
-
-{if($config->debug)}
-    {!js::import($jsRoot . 'jquery/min.js')}
-    {!js::import($jsRoot . 'zui/min.js')}
-    {!js::import($jsRoot . 'chanzhi.js')}
-    {!js::import($jsRoot . 'my.js')}
-    {!css::import($webRoot . 'zui/css/min.css')}
-    {!css::import($themeRoot . 'common/style.css')}
-{else}
-    {if($cdnRoot)}
-        {!css::import($cdnRoot . '/theme/default/default/chanzhi.all.css', '', $version = false)}
-        {!js::import($cdnRoot  . '/js/chanzhi.all.js', $version = false)}
-        {else}
-        {!css::import($themeRoot . 'default/chanzhi.all.css')}
-        {!js::import($jsRoot     . 'chanzhi.all.js')}
-    {/if}
-{/if}
-{if(file_exists($customCssFile))}
-{!css::import($customCssURI, "id='themeStyle'")}
-{/if}
-{if(isset($pageCSS))}
-{!css::internal($pageCSS)}
-{/if}
-{!html::icon($favicon)}
-{!html::rss(helper::createLink('rss', 'index', '', '', 'xml'), $config->site->name)}
-
-<!--[if lt IE 9]>
-  {if($config->debug)}
-  	{!js::import($jsRoot . 'html5shiv/min.js')}
-  	{!js::import($jsRoot . 'respond/min.js')}
-  {else}
-  	{if($cdnRoot)}
-	  <link href="{$cdnRoot}/js/respond/cross-domain/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
-      <link href="/js/respond/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
-      {!js::import($jsRoot . 'html5shiv/min.js')}
-      {!js::import($jsRoot . 'respond/min.js')}
-      {!js::import($jsRoot . 'respond/cross-domain/respond.proxy.js')}
-  	{else}
-	  {!js::import($jsRoot . 'chanzhi.all.ie8.js')}
-  	{/if}
-  {/if}
-<![endif]-->
-<!--[if lt IE 10]>
-  {if($config->debug)} {!js::import($jsRoot . 'jquery/placeholder/min.js')} {/if}
-  {if(!$config->debug)} {!js::import($jsRoot . 'chanzhi.all.ie9.js')} {/if}
-<![endif]-->
-
-{!js::set('lang', $lang->js)}
-{if(!empty($config->oauth->sina) and !is_object($config->oauth->sina))}
-  {$sina=json_decode($config->oauth->sina)}
-{/if}
-{if(!empty($config->oauth->qq) and !is_object($config->oauth->qq))}
-  {$qq=json_decode($config->oauth->qq)}
-{/if}
-{if(!empty($sina->verification))} {$sina->verification} {/if}
-{if(!empty($qq->verification))} {$qq->verification} {/if}
-{if(!empty($sina->widget))}
-{!js::import('https://tjs.sjs.sinajs.cn/open/api/js/wb.js')}
-{/if}
-{$baseCustom=isset($config->template->custom) ? json_decode($config->template->custom, true) : array()}
-{if(!empty($baseCustom[CHANZHI_TEMPLATE][CHANZHI_THEME]['js']))}
-{!js::execute($baseCustom[CHANZHI_THEME][CHANZHI_THEME]['js'])}
-{/if}
-{$control->block->printRegion($layouts, 'all', 'header')}
-</head>
-<body>
-{if(isset($resultCustomCss) and $resultCustomCss['result'] != 'success')}
-  {if(!empty($resultCustomCss['message']))}
-    <div class='alert alert-danger'> {$lang->customCssError;} </div>
-  {/if}
-{/if}
+<html><!--PHP-->
+<head>
+	<title>R376</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<meta name='renderer' content='webkit'>
+	<meta http-equiv='X-UA-Compatible' content='IE=Edge,chrome=1' >
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=no"/>		<meta name="format-detection" content="telephone=no"/>	<meta name="keywords" content=""/>
+	<meta name="description" content=""/>
+			<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/scripts/swiper.animate/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/bootstrap.min.css"  />
+	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/ModuleStyleMobile.css?v=20171012"/>
+		<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/ModuleMobileNavTpl.css" />
+	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/fontawesome/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/scripts/iconfont/iconfont.css" />
+	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/datetimepicker/jquery.datetimepicker.css"/>
+	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/skinp/R376/skinsrc/styles/style.css"/>
+	<!--[if IE 9]>
+	<link rel="stylesheet" href="http://cdn.yz168.cc/scripts/swiper2/css/idangerous.swiper.css">
+	<script type="text/javascript" src="http://cdn.yz168.cc/scripts/swiper2/js/idangerous.swiper.min.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/scripts/Swiper-3.4.0/css/swiper.min.css"/>
+	<script type="text/javascript" src="http://cdn.yz168.cc/scripts/Swiper-3.4.0/js/swiper.min.js"></script>
+	<script type="text/javascript" src="http://cdn.yz168.cc/scripts/swiper.animate/swiper.animate1.0.2.min.js"></script>
+	<link href='http://cdn.yz168.cc/PageCss.css.php?id=901_310701_1&pagecacheid=PageCache_901_310701_1_cn_cn&siteid=901&md5=11909582e23ec3310968d3410e69e568' type='text/css' rel='stylesheet'/>	<!--<script src="http://cdn.yz168.cc/share/jquery.min.js" type="text/javascript"></script>-->
+	<!--<script src="http://cdn.yz168.cc/scripts/flexslider/jquery.flexslider-min.js" type="text/javascript" charset="utf-8"></script>-->
+	<script>
+        var SiteType = "1"; var CanDesign = "False"; var CanEditFront = 'False'; var SkinType = "3" || "2";var GridWidth = "1200px";
+        var IsWeiXin = navigator.userAgent.toLowerCase().match(/MicroMessenger/i) == 'micromessenger';
+	</script>
+	<script src="http://cdn.yz168.cc/PageJs.js.php?act=MobileJs&v=2018012015541"></script>
+	<script>
+        loadStyleSheet('/share/jquery-ui-css/ui-lightness/jquery-ui-1.10.2.custom.min.css',CanDesign != 'True'); //非编辑模式下延时加载
+	</script>
+	</head>
+<body id="page_310701" class="module_bg_color">
