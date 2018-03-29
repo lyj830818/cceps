ad9715a20d50c5851a82940967e5a3ef5529ebc6
lyj <29422882@qq.com>
2018-03-29 21:46:50 +0800

    modify

--- system/tmp/template/default/common/header.lite.html.php
+++ system/tmp/template/default/common/header.lite.html.php
@@ -7,30 +7,29 @@
 	<meta http-equiv='X-UA-Compatible' content='IE=Edge,chrome=1' >
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=no"/>		<meta name="format-detection" content="telephone=no"/>	<meta name="keywords" content=""/>
 	<meta name="description" content=""/>
-			<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/scripts/swiper.animate/animate.min.css">
-	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/bootstrap.min.css"  />
-	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/ModuleStyleMobile.css?v=20171012"/>
-		<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/ModuleMobileNavTpl.css" />
-	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/fontawesome/css/font-awesome.min.css" />
-	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/scripts/iconfont/iconfont.css" />
-	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/share/datetimepicker/jquery.datetimepicker.css"/>
-	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/skinp/R376/skinsrc/styles/style.css"/>
+			<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/scripts/swiper.animate/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/share/bootstrap.min.css"  />
+	<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/share/ModuleStyleMobile.css?v=20171012"/>
+		<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/share/ModuleMobileNavTpl.css" />
+	<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/share/fontawesome/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/scripts/iconfont/iconfont.css" />
+	<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/share/datetimepicker/jquery.datetimepicker.css"/>
+	<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/skinp/R376/skinsrc/styles/style.css"/>
 	<!--[if IE 9]>
-	<link rel="stylesheet" href="http://cdn.yz168.cc/scripts/swiper2/css/idangerous.swiper.css">
-	<script type="text/javascript" src="http://cdn.yz168.cc/scripts/swiper2/js/idangerous.swiper.min.js"></script>
+	<link rel="stylesheet" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/scripts/swiper2/css/idangerous.swiper.css">
+	<script type="text/javascript" src="/r376.mdemo.yz168.cc/cdn.yz168.cc/scripts/swiper2/js/idangerous.swiper.min.js"></script>
 	<![endif]-->
-	<link rel="stylesheet" type="text/css" href="http://cdn.yz168.cc/scripts/Swiper-3.4.0/css/swiper.min.css"/>
-	<script type="text/javascript" src="http://cdn.yz168.cc/scripts/Swiper-3.4.0/js/swiper.min.js"></script>
-	<script type="text/javascript" src="http://cdn.yz168.cc/scripts/swiper.animate/swiper.animate1.0.2.min.js"></script>
-	<link href='http://cdn.yz168.cc/PageCss.css.php?id=901_310701_1&pagecacheid=PageCache_901_310701_1_cn_cn&siteid=901&md5=11909582e23ec3310968d3410e69e568' type='text/css' rel='stylesheet'/>	<!--<script src="http://cdn.yz168.cc/share/jquery.min.js" type="text/javascript"></script>-->
-	<!--<script src="http://cdn.yz168.cc/scripts/flexslider/jquery.flexslider-min.js" type="text/javascript" charset="utf-8"></script>-->
+	<link rel="stylesheet" type="text/css" href="/r376.mdemo.yz168.cc/cdn.yz168.cc/scripts/Swiper-3.4.0/css/swiper.min.css"/>
+	<script type="text/javascript" src="/r376.mdemo.yz168.cc/cdn.yz168.cc/scripts/Swiper-3.4.0/js/swiper.min.js"></script>
+	<script type="text/javascript" src="/r376.mdemo.yz168.cc/cdn.yz168.cc/scripts/swiper.animate/swiper.animate1.0.2.min.js"></script>
+	<link href='/r376.mdemo.yz168.cc/cdn.yz168.cc/PageCss.css' type='text/css' rel='stylesheet'/>
 	<script>
         var SiteType = "1"; var CanDesign = "False"; var CanEditFront = 'False'; var SkinType = "3" || "2";var GridWidth = "1200px";
         var IsWeiXin = navigator.userAgent.toLowerCase().match(/MicroMessenger/i) == 'micromessenger';
 	</script>
-	<script src="http://cdn.yz168.cc/PageJs.js.php?act=MobileJs&v=2018012015541"></script>
+	<script src="/r376.mdemo.yz168.cc/cdn.yz168.cc/PageJs.js"></script>
 	<script>
-        loadStyleSheet('/share/jquery-ui-css/ui-lightness/jquery-ui-1.10.2.custom.min.css',CanDesign != 'True'); //非编辑模式下延时加载
+        loadStyleSheet('/r376.mdemo.yz168.cc/share/jquery-ui-css/ui-lightness/jquery-ui-1.10.2.custom.min.css',CanDesign != 'True'); //非编辑模式下延时加载
 	</script>
 	</head>
 <body id="page_310701" class="module_bg_color">
